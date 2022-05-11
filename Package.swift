c3e8250e4a733b60a4174214f380a2dcd51d7ef1
soundflix <80773469+soundflix@users.noreply.github.com>
2022-05-11 19:44:55 +0200

    updated package targets

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let package = Package(
     name: "SwiftOSC",
 	 platforms: [
 			  .macOS(.v10_15),
-			  .iOS(.v12),
-			  .watchOS(.v5)
+			  .iOS(.v13),
+			  .watchOS(.v6)
 		 ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
