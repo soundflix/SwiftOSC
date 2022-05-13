ee60e281beeabf401a8396de23c74032ada1be91
soundflix <80773469+soundflix@users.noreply.github.com>
2022-05-13 17:29:19 +0200

    changed dispatch QoS, safe host string, cleanup/cosmetics

--- Framework/SwiftOSC/Network/OSCClient.swift
+++ Framework/SwiftOSC/Network/OSCClient.swift
@@ -12,7 +12,7 @@ import Network
 public class OSCClient {
 
     public var connection: NWConnection?
-    var queue: DispatchQueue
+    var queue: DispatchQueue = DispatchQueue(label: "SwiftOSC Client", qos: .userInteractive)
     public var serviceType: String? = "_osc._udp"
     public var browser: NWBrowser?
 
@@ -21,63 +21,38 @@ public class OSCClient {
     
     public init(serviceType:String) {
             self.serviceType = serviceType
-//            super.init()
-        queue = DispatchQueue(label: "SwiftOSC Client") // better use .main ?
+
             self.startBrowsing()
         }
 
     public init(host: String, port: UInt16) {
+        var safeHost = host
+        /// defaut to localhost (empty string will crash)
+        if safeHost == "" {
+            safeHost = "127.0.0.1"
+        }
+        self.host = NWEndpoint.Host(safeHost)
 
-        self.host = NWEndpoint.Host(host)
-//        if (1 ... 65535).contains(port) {
-//            self.port = NWEndpoint.Port(rawValue: port)!
-//        } else {
-//            self.port = NWEndpoint.Port(rawValue: 1)!
-//        }
         self.port = NWEndpoint.Port(rawValue: port) ?? NWEndpoint.Port.any
 
-        queue = DispatchQueue(label: "SwiftOSC Client")
         setupConnection()
     }
 
-    func setupConnection(){
+    /// setup a new connection
+    func setupConnection() {
 
         let params = NWParameters.udp
-        params.serviceClass = .signaling // DEBUG: test if this is really faster than '.best-effort'
+        params.serviceClass = .signaling // DEBUG: test if this is effectively faster than '.best-effort'
         
-        // create the connection
+        /// create the connection
         if let host = self.host, let port = self.port {
             connection = NWConnection(host: host, port: port, using: params)
-            // TODO make custom connection class to reuse with bonjour variant
         }
             
-        // setup state update handler
+        /// setup state update handler
         connection?.stateUpdateHandler = stateUpdateHandler(newState:)
-//        connection?.stateUpdateHandler = { [weak self] (newState) in
-//            switch newState {
-//            case .ready:
-//                NSLog("SwiftOSC Client is ready. \(String(describing: self?.connection))")
-//            case .failed(let error):
-//                NSLog("SwiftOSC Client failed with error \(error)")
-//                NSLog("SWiftOSC Client is restarting.")
-//                self?.setupConnection()
-//            case .cancelled:
-//                NSLog("SWiftOSC Client cancelled.")
-//                break
-//            case .waiting(let error):
-//                NSLog("SwiftOSC Client waiting with error \(error)")
-//            case .preparing:
-//                NSLog("SWiftOSC Client is preparing.")
-//                break
-//            case .setup:
-//                NSLog("SWiftOSC Client is setting up.")
-//                break
-//            @unknown default:
-//            fatalError()
-//            }
-//        }
 
-        // start the connection
+        /// start the connection
         connection?.start(queue: queue)
 
     }
@@ -121,11 +96,9 @@ public class OSCClient {
         }))
     }
     public func restart() {
-        // destroy connection and listener
-        connection?.forceCancel()
+//        connection?.forceCancel()
+        connection?.cancel()
 
-
-        // setup new listener
         setupConnection()
     }
 }
